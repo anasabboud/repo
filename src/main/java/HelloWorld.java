9cd58bf794450367467d20b74fc87a3acd493a22
Anas El Abboud <anas.elabboud@tui.com>
2018-01-31 16:48:51 +0100

    update zustand

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,24 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
 public class HelloWorld{
+
+  private static final Logger LOG = Logger.getLogger(HelloWorld.class.getName());
 
   public static void main(String[] args) throws Exception{
  
     System.out.println("HelloWorld");
 
+  }
+
+  @RequestMapping("/aela2")
+  public String forward() {
+    LOG.log(Level.INFO , "aela2 has been called");
+    return "aela2 has been called";
   }
 
 }
